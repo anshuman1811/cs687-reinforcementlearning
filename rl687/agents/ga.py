aff641e6bc1a59b2c91a8536ad25d12de6d98e87
Anshuman Mishra <anshuman@1x-nat-vl931-172-30-142-50.wireless.umass.edu>
2019-10-08 21:10:39 -0400

    (fix) Reverting ga.py

--- rl687/agents/ga.py
+++ rl687/agents/ga.py
@@ -1,9 +1,6 @@
 import numpy as np
 from .bbo_agent import BBOAgent
-from multiprocessing.dummy import Pool
 from typing import Callable
-
-
 class GA(BBOAgent):
     """
     A canonical Genetic Algorithm (GA) for policy search is a black box 
@@ -26,7 +23,6 @@ class GA(BBOAgent):
                     to be copied (unmodified) to the next generation
     
     """
-
     def __init__(self, populationSize:int, evaluationFunction:Callable, 
                  initPopulationFunction:Callable, numElite:int=1, numTruncate:int=1, alpha:float=0.5, numEpisodes:int=10):
         print ("Init")
@@ -42,6 +38,25 @@ class GA(BBOAgent):
         self.numEpisodes = numEpisodes
         self.initPopulationFunction = initPopulationFunction
 
+        for candidate in self._population:
+            print ("Evaluating Candidate")
+            J = self.evaluationFunction(candidate, self.numEpisodes)
+            print ("Return", J)
+            if J > self.bestReturn:
+                print ("Better Policy Found!")
+                self.bestReturn = J
+                self._parameters = candidate
+        print (self._population, self._parameters, self.bestReturn)
+        # for candidate in self._population:
+        #     print ("Evaluating Candidate")
+        #     J = self.evaluationFunction(candidate, self.numEpisodes)
+        #     print ("Return", J)
+        #     if J > self.bestReturn:
+        #         print ("Better Policy Found!")
+        #         self.bestReturn = J
+        #         self._parameters = candidate
+        # print (self._population, self._parameters, self.bestReturn)
+
         self.numTruncate = numTruncate
         self.alpha = alpha
 
@@ -52,7 +67,6 @@ class GA(BBOAgent):
     @property
     def parameters(self)->np.ndarray:
         #TODO
-        print("Querying params")
         return self._parameters
 
     def _mutate(self, parent:np.ndarray)->np.ndarray:
@@ -77,17 +91,6 @@ class GA(BBOAgent):
     def train(self)->np.ndarray:
         returns = []
         print ("Training")
-        # print ("Creating Pool")
-        # threadPool = Pool(self.populationSize)
-        # print ("Evaluating in parallel")
-        # returns = threadPool.starmap(self.evaluationFunction, [(candidate, self.numEpisodes) for candidate in self._population])
-        # print("Returns", returns)
-        # print ("Threads completed")
-        # returns = [(self._population[i], J) for i,J in enumerate(returns)]
-        # threadPool.close()
-        # threadPool.join()
-        # for candidate,J in returns:
-        returns = []
         for candidate in self._population:
             print ("Evaluating Candidate")
             J = self.evaluationFunction(candidate, self.numEpisodes)
@@ -98,20 +101,24 @@ class GA(BBOAgent):
                 self.bestReturn = J
                 self._parameters = candidate
 
-        # print("Sorting Candidates")
-        sortedCandidates = [candidate for candidate,J in sorted(returns, key=lambda tup:tup[1], reverse=True)]
-        # print("Getting Parents")
+        print("Sorting Candidates")
+        sortedCandidates = [candidate[0] for candidate in sorted(returns, key=lambda tup:tup[1], reverse=True)]
+        print("Getting Parents")
+
         parents = self.getParents(self.numTruncate, sortedCandidates)
-        # print("Extracting Elite Candidates")
+        
+        print("Extracting Elite Candidates")
         elite = sortedCandidates[:self.numElite]
         # print(len(elite))
+        
         print("Getting Children")
         children = self.getChildren(self.alpha, parents)
         # print (len(children))
+        
         self._population[:self.numElite] = elite
         self._population[self.numElite:] = children
-        # print ("New Population", len(self._population), self._population[0].shape)
-        # print ("Best params", np.array(sortedCandidates[0]).shape)
+        print ("New Population", len(self._population), self._population[0].shape)
+        print ("Best params", np.array(sortedCandidates[0]).shape)
         return sortedCandidates[0]
 
     def reset(self)->None:
